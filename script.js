b44fe2e2133a5417bde581316b82d8eb5fbd5c91
NOAH SMART <noahsmart48@gmail.com>
2024-07-10 22:04:40 +0100

    done

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const myBtn = document.getElementById("myBtn");
 
-myBtn.addEventListener("click", function() {
-    const faveCar = prompt("What is your favourite car?");
-    console.log(faveCar)
+myBtn.addEventListener("click", function () {
+  const faveCar = prompt("What is your favourite car?");
+  console.log(faveCar);
 });
 
 const myBtn2 = document.getElementById("myBtn2");
 
-myBtn2.addEventListener("click", function() {
-    const faveColour = prompt("What is your favourite colour on a car?");
-    console.log(faveColour)
+myBtn2.addEventListener("click", function () {
+  const faveColour = prompt("What is your favourite colour on a car?");
+  console.log(faveColour);
 });
 
 const myBtn3 = document.getElementById("myBtn3");
 
-myBtn3.addEventListener("click", function() {
-    const carBrand = prompt("What is your favourite car brand?");
-    console.log(carBrand)
+myBtn3.addEventListener("click", function () {
+  const carBrand = prompt("What is your favourite car brand?");
+  console.log(carBrand);
 });
